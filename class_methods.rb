7b6f1e92f64f0f2c6b77332bb341a745ad685516
Trish Griego <pmgriego@gmail.com>
2016-08-24 10:32:18 -0700

    Ada Week 3, Day2 working on understanding of class methods like self. Lots of great examples and notes.

--- class_methods.rb
+++ class_methods.rb
@@ -26,13 +26,31 @@
 ###################################
 
 ## INSTANCE VARIABLES
+    ## ATTRIBUTES
+    ##  Ex: username , email, age, something that all objects might have. Each one will have specific details.
 
 ## INSTANCE METHODS
+    ## behaviors
+      ## ex: make a post, send a message, like a post, poke someone, create a photo album, the things that each object is allowed to do.
 
-      #INITIALIZE METHOD
-
-
-## ATTRIBUTES
+    ## INITIALIZE METHOD
+    # actions that are preformed every time the class is set up.
+# class User
+#   # sometimes if you just one to place them here because you know you're going to use them.
+#
+#
+#   def initialize
+#     @name
+#   end
+#
+#   def add_username(name)
+#     @name = name
+#   end
+# end
+#
+# user1 = User.new
+# puts user1.methods
+# gives you a list of all the methods.
 
 
 # class Order
@@ -61,8 +79,20 @@
 ###################################
 
 #Class methods are called directly by the class and not by an instance of the class.
-
-
+# class Kitten
+# ##############################################
+#   # typically how we write it.
+#   # def say_meow
+#   #   puts "meow"
+#   # end
+#   # if you wanted to use this later. You should use return.
+# ##############################################
+#   def self.say_meow
+#     return "meow"
+#   end
+# end
+#
+# puts Kitten.say_meow
 
 
 
@@ -71,41 +101,53 @@
 ###################################
 
 
-# class Pawn
-#   attr_reader :position
-#   def initialize(position)
-#     @position = position
-#   end
+class Pawn
+  attr_reader :position
+  def initialize(position)
+    @position = position
+  end
+
+  # This is the class method, it starts with self.
+  # It is only called on the class directly Pawn.make_row
+  def self.make_row(side)
+    if side == "white"
+      num = 2
+    else
+      num = 7
+    end
+
+    pawns = []
+    ("a".."h").each do |letter|
+      # Spawning itself...
+      # a new object of pawn with letter and number associated.
+      # taking in one these leters and number from above.
+      pawns << self.new("#{letter}#{num}")
+    end
+
+    pawns
+  end
+end
 #
-#   # This is the class method, it starts with self.
-#   # It is only called on the class directly Pawn.make_row
-#   def self.make_row(side)
-#     if side == "white"
-#       num = 2
-#     else
-#       num = 7
-#     end
+#make one pawn
+one_pawn = Pawn.new("A2")
+one_pawn.make_row("purple")
 #
-#     pawns = []
-#     ("a".."h").each do |letter|
-#       pawns << self.new("#{letter}#{num}")
-#     end
-#
-#     pawns
-#   end
-# end
-#
-# #make one pawn
-# one_pawn = Pawn.new("A2")
-#
-# #make a whole row of pawns
+#make a whole row of pawns
 # pawns = Pawn.make_row("black")
 #
 # #What is being stored in this local variable pawns?
 # print pawns
 #
+# pawns.each do |pawn|
+#   puts pawn.position
+# end
+
 # #WHAT IS THIS DOING!?
 # puts pawns.shuffle.first.position
+
+# like to go over what each of these are again.
+# p
+
 
 
 
@@ -113,6 +155,7 @@
 ######   WHERE IS THIS USED?  #####
 ###################################
 #Class methods are for anything that does not deal with an individual instance of a class
+## need to think about this more. I wouldnt want to make a class method for send message.
 
 
 #In Gems, like faker
